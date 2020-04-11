8d93ddf112bde7f5c649d877bfe71646551a9e40
Ilona <hair.masterpiece@gmail.com>
2020-04-11 12:29:12 +0300

    создание главной страницы,создали файлы Quiz,добавили css свойство градиент (backround:linear-gradient()),добавила джс файл создала список вопросав.создала css свойства .которые пнадобятся для своего  проекта.обработка клика,отображение состояние обьекта. сделал обработчик событий вычисления результата Кнопка переключения меню Реализация меню Компонент затемнение Установка и настройка

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,37 +1,45 @@
 import React, { Component } from 'react';
-import classes from './Drawer.module.css'
+import classes from './Drawer.module.css';
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 
-const links =[
- 1, 2, 3
+const links = [
+    1, 2, 3
 ];
 
 class Drawer extends Component {
+    isOpen;
     
     renderLinks() {
-     return links.map((link, index) => {
-      return (
-          <li key={index}>
-           <a>Link {link}</a>
-          </li>
-      )
-     })
+        return links.map((link, index) => {
+            return (
+                <li key={index}>
+                    <a>Link {link}</a>
+                </li>
+            );
+        });
     }
     
     render() {
-        const cls =[classes.Drawer];
+        const cls = [classes.Drawer];
         
         if (!this.props.isOpen)
-          cls.push(classes.close)
+            cls.push(classes.close);
         
         return (
-          <nav className={cls.join(' ')}>
-              <ul>
-                  {this.renderLinks()}
-              </ul>
-          </nav>
+            <React.Fragment>
+                <nav className={cls.join(' ')}>
+                    <ul>
+                        {this.renderLinks()}
+                    </ul>
+                </nav>
+                {this.props.isOpen
+                    ? <Backdrop onClick={this.props.onClose}/>
+                    : null
+                }
+            </React.Fragment>
         );
     }
 }
 
-export default Drawer
+export default Drawer;
