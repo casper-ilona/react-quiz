a29e1b54c9372bc01160385c1a5fa8b8c19bcbcb
Ilona <hair.masterpiece@gmail.com>
2020-04-21 13:12:59 +0300

    Создание контролов

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import classes from './QuizCreator.module.css';
+import Button from '../../components/UI/Button/Button';
 
 export class QuizCreator extends Component {
+    
+    submitHandler = event => {
+        event.preventDefault();
+    };
+    
+    addQuestionHandler = () => {
+    
+    };
+    
+    createQuizHandler = () => {
+    
+    };
+    
+    
     render() {
         return (
-            <div>
-              <h1>Quiz Creator</h1>
+            <div className={classes.QuizCreator}>
+                <div>
+                    <h1>Create test</h1>
+                    
+                    <form onSubmit={this.submitHandler}>
+                        
+                        <input type="text"/>
+                        <hr/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                        <input type="text"/>
+                            
+                            
+                            <select></select>
+                            
+                            <Button
+                                type="primary"
+                                onClick={this.addQuestionHandler}
+                            >
+                                Add Question
+                            </Button>
+                            <Button
+                                type="success"
+                                onClick={this.createQuizHandler}
+                            >
+                                Create test
+                            </Button>
+                    </form>
+                    
+                </div>
+                
             </div>
-        );
+    );
     }
-}
+    }
